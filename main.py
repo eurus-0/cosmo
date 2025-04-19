c92fc757d529dd65793a23ca96853c84a4219d08
eurus-0 <eurus.media0@gmail.com>
2025-04-19 20:56:00 +0530

    main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import os
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 4000))
     app.run(host="0.0.0.0", port=port)
 from app import app  # noqa: F401
 from routes import *  # noqa: F401
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=4000, debug=True)
