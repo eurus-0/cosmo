d0aaad72e4248cbf2f359164bd21617402301ce2
eurus-0 <eurus.media0@gmail.com>
2025-04-19 20:34:06 +0530

    main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-from routes import init_routes
-
-app = Flask(__name__)
-init_routes(app)
+import os
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
