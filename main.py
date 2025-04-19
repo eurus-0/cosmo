9e8c60c3232f8b4930f42abf2936e6673a2b5f4a
eurus-0 <eurus.media0@gmail.com>
2025-04-19 20:26:57 +0530

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from app import app  # noqa: F401
-from routes import *  # noqa: F401
+from flask import Flask
+from routes import init_routes
+
+app = Flask(__name__)
+init_routes(app)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
